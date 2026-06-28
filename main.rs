8eb99e8b996f98ee22e1316fc550765bbf61ed2c
samylayang <samylayang@gmail.com>
2026-06-28 19:15:28 +0900

    1st commit

---
+++ main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let mut s = String::from("Hello"); // Create a new String with content "Hello"
+    s.push_str(", world!"); // Append the string ", world!" to the original String
+    println!("{}", s); // Output: "Hello, world!"
+}
